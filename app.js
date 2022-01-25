9cedf814ec1dd312dc1e56781620927b480d06c8
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-01-25 14:46:31 -0300

    1er form agregado

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 
 app.listen(3030, ()=>{
@@ -9,4 +12,8 @@ app.listen(3030, ()=>{
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
 });
