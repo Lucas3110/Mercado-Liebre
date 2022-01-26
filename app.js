a78699380e93de54e4328d9462805c3b3caecf81
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-01-26 11:14:42 -0300

    clase 15 microdesafios hechos con validacion + forms

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.listen(3030, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'))
