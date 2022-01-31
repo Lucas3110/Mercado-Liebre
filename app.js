77d02dc8cb8c34da6f274710d3d4dac428b3a199
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-01-31 12:08:41 -0300

    Todo listo para levantar en Heroku

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 
-app.listen(3030, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto 3000")
+})
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
